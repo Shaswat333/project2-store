e84cfde0a57709f58e1cfeda47bb79e668ae6ab1
Shaswat333 <shaswatpachhera21@gmail.com>
2022-12-17 16:59:38 +0000

    poject2-store

--- routes/store.routes.js
+++ routes/store.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Store = require('../models/Store.model');
+const Store = require('../models/store.model');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 // ------ CRUD - Create -------
